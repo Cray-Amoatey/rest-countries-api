c93f275452525b880be5767ecbfa57234f367b75
Cray-Amoatey <amoatey.cray@gmail.com>
2023-04-21 08:29:33 +0000

    installed styled-components library for styled components

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+height:90px;
+box-shadow:2px 2px 1em rgba(0,0,0)
+
+.container {
+    display: flex;
+    width: 1024px;
+    margin: auto;
+    justify-content: space-between;
+    align-itmes: center;
+
+    p{
+        cursor: pointer
+    }
+}
+
+
+ `
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Routes} from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+   <Routes>
+    <App/>
+   </Routes>
   </React.StrictMode>,
 )
