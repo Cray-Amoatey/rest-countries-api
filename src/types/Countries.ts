735a746eac49322e01e9f20156ebbea447aebb5a
Cray-Amoatey <amoatey.cray@gmail.com>
2023-04-22 12:52:25 +0000

    country information interface

---
+++ src/types/Countries.ts
@@ -0,0 +1,10 @@
+export interface CountriesTs {
+    name: string,
+    capital: string,
+    population: string,
+    region: string,
+    numericCode:number,
+    flags: {
+        png: string
+    }
+}
