7d1258c2f92edf22ffda154840c0e8076ca0468e
Amoatey <emmanuel.amoatey@amalitech.org>
2023-06-05 13:30:55 +0000

    recoded using api context included necessary icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { MainRoutes } from "./Routes/MainRoutes";
-import { Header } from "./components/Header";
-
+import React from "react";
+import { useContext } from "react";
+import Countries from "./components/Countries";
+import Header from "./components/Header";
+import Country from "./components/Country";
+import PageNotFound from "./components/PageNotFound";
+import { Routes, Route } from "react-router-dom";
+import { nightModeContext } from "./context/ThemeContext";
 
 function App() {
-  return(
-    <>
-    <Header/> 
-    <MainRoutes/>
-    </>
+  const { dark } = useContext(nightModeContext);
+
+  return (
+    <div className={dark ? "dark" : "light"}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Countries />} />
+        <Route path=":countryName" element={<Country />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/types/input.ts
+++
@@ -1,5 +0,0 @@
-export interface InputTS {
-  value: string;
-  setSearch: (e: string) => void,
-  setRegion: (e: string) => void,
-}    
